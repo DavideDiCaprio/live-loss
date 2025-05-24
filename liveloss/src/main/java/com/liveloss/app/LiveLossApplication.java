945b3598f862ef21ca388f336ebfb293cfed69f2
Davide Di Caprio <dicapriodavide@outlook.com>
2025-05-24 18:10:49 +0000

    Add initial Spring Boot project structure

---
+++ liveloss/src/main/java/com/liveloss/app/LiveLossApplication.java
@@ -0,0 +1,13 @@
+package com.liveloss.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveLossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveLossApplication.class, args);
+	}
+
+}
---
+++ liveloss/src/test/java/com/liveloss/app/LiveLossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liveloss.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveLossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
