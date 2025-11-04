2617579a02d512a6844510a4484c973576ad8842
Davide Di Caprio <93427056+DavideDiCaprio@users.noreply.github.com>
2025-11-04 20:33:29 +0000

    fix settings.py

--- app/settings.py
+++ app/settings.py
@@ -1,10 +1,20 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
+    """Encapsulates all application configuration settings.
+
+    Attributes:
+        APP_TITLE (str): The title of the application, used in OpenAPI docs.
+        APP_DESCRIPTION (str): The description of the application, used in OpenAPI docs.
+        DATABASE_URL (str): The connection string for the primary database
+        REDIS_URL (str): The connection string for the Redis instance
+    """
     APP_TITLE: str = "live-loss API"
     APP_DESCRIPTION: str = "live-Loss project"
     DATABASE_URL: str = ""
-    REDIS_URL: str = "' 
+    REDIS_URL: str = ""
 
+    class Config:
+        pass
 
 settings = Settings()
